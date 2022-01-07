9e5ae014f7ea790c3a2e79a3e28d2813ceab98d3
Kali <kalijonn.js@gmail.com>
2022-01-07 08:25:36 +0530

    react app scaffolding

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+ReactDOM.render(
+	<h1 className='bg-slate-200'>Hello</h1>,
+	document.getElementById('root'),
+);
